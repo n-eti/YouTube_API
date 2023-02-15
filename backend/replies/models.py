9be74695a1f5f584094a3c4e69813fdd75678427
Nada Atieh <nada.s.atieh@gmail.com>
2023-02-15 13:30:19 -0600

    Created the replies app + updated MySQL Workbench tables

--- backend/replies/models.py
+++ backend/replies/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from authentication.models import User
 # Create your models here.
+
+class Reply(models.Model):
+    user = models.ForeignKey(User, related_name = 'reply', on_delete=models.CASCADE)
+    comment = models.ForeignKey(User, related_name = 'text', on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
