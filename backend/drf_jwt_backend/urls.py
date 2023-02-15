23a1ef6dd35511d67ddaf0aa2b59e92d3b08ad99
Aubrey Guishard <aubrey.guishard@gmail.com>
2023-02-15 15:35:27 -0500

    Created url pathway for Comments App

--- backend/drf_jwt_backend/urls.py
+++ backend/drf_jwt_backend/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('api/auth/', include('authentication.urls')),
     path('api/cars/', include('cars.urls')),
     path('api/replies/', include('replies.urls')),
+    path('api/comments/', include('comments.urls')),
 ]
