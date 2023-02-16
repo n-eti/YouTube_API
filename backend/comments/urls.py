91f2f256e865b98afe811403454db13991da6733
Aubrey Guishard <aubrey.guishard@gmail.com>
2023-02-16 14:06:53 -0500

    Imported authentication into the views file in the comment app + created the POST endpoint

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<str:video_id>/', views.comments_list),
+    path('video/<str:video_id>/', views.comments_list),
+    path('', views.add_comment),
+    path('<int:comment_id>/', views.edit_comment),
 ]
--- backend/comments/views.py
+++ backend/comments/views.py
@@ -1,11 +1,14 @@
+from rest_framework import status
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from .models import Comment
 from .serializers import CommentSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
 # Create your views here.
 
 @api_view(['GET'])
+@permission_classes([AllowAny])
 def comments_list(request, video_id):
     comments = Comment.objects.filter(video_id = video_id)
 
@@ -13,3 +16,20 @@ def comments_list(request, video_id):
 
 
     return Response(serializer.data)
+
+@api_view(['PUT'])
+def edit_comment(request, comment_id):
+    return Response(comment_id)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def add_comment(request):
+    print(request)
+    serializer = CommentSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save(user=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+
