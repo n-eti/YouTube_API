a8279dbdbbcea25c47146b0cb5a4190c88eb8caf
Nada Atieh <nada.s.atieh@gmail.com>
2023-02-17 11:46:30 -0600

    Updated Replies app views, models, and serializers files

--- backend/replies/views.py
+++ backend/replies/views.py
@@ -2,7 +2,11 @@ from rest_framework import status
 from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+
 from .models import Reply
+
+# from .models import Comment
+
 from .serializers import ReplySerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 # Create your views here.
@@ -19,4 +23,5 @@ def replies_list(request, comment_id):
         serializer.is_valid(raise_exception=True)
         serializer.save(comment_id=comment_id, user=request.user)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-            
+
+        
