0557d385ac0bb19941dd9d3a627735bd0728822a
Aubrey Guishard <aubrey.guishard@gmail.com>
2023-02-21 14:58:00 -0500

    Created logic for the VideoPlayer File

--- frontend/src/components/VideoPlayer/VIdeoPlayer.jsx
+++ frontend/src/components/VideoPlayer/VIdeoPlayer.jsx
@@ -1,7 +1,16 @@
-const VideoPlayer = ({}) => {
-    return ( <div>
-        <iframe src={``} ></iframe>
-    </div> );
-}
- 
+const VideoPlayer = ({ videoId }) => {
+  return (
+    <div>
+      <iframe
+        id="ytplayer"
+        type="text/html"
+        width="640"
+        height="360"
+        src={``}
+        frameborder="0"
+      ></iframe>
+    </div>
+  );
+};
+
 export default VideoPlayer;
