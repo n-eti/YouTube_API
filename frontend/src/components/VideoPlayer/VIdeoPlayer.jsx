f7c3fd7359856794354aeb6ff094951af4ef9f7d
Aubrey Guishard <aubrey.guishard@gmail.com>
2023-02-22 10:35:58 -0500

    Instantiated Video player inside of the VideoPage + Was able to get the selected video on the search page to display on the video page

--- frontend/src/components/VideoPlayer/VIdeoPlayer.jsx
+++ frontend/src/components/VideoPlayer/VIdeoPlayer.jsx
@@ -1,17 +1,14 @@
 import { useParams } from "react-router-dom";
 
-const VideoPlayer = ({}) => {
-let {videoId} = useParams();
+const VideoPlayer = ({videoId}) => {
+// let {videoId} = useParams();
 
   return (
-    <div>
+    <div className='video-player'>
       <iframe
-        id="ytplayer"
-        width="640"
-        height="360"
         src={`https://www.youtube.com/embed/${videoId}`}
-        frameborder="0"
       ></iframe>
+      <h3></h3>
     </div>
   );
 };
--- frontend/src/pages/VideoPage/VideoPage.jsx
+++ frontend/src/pages/VideoPage/VideoPage.jsx
@@ -1,9 +1,10 @@
 import { useParams } from "react-router-dom";
+import VideoPlayer from "../../components/VideoPlayer/VIdeoPlayer";
 
 const VideoPage = ({}) => {
   const { videoId } = useParams();
   console.log(videoId);
-  return <div><VideoPage/></div>;
+  return <div><VideoPlayer/></div>;
 };
 
 export default VideoPage;
