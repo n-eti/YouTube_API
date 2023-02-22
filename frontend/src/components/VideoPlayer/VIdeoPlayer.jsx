1399905edbae43975c5ebfd291b2db01846a1541
Aubrey Guishard <aubrey.guishard@gmail.com>
2023-02-22 09:14:08 -0500

    Working on the video page to display selected video

--- frontend/src/components/VideoPlayer/VIdeoPlayer.jsx
+++ frontend/src/components/VideoPlayer/VIdeoPlayer.jsx
@@ -1,12 +1,15 @@
-const VideoPlayer = ({ videoId }) => {
+import { useParams } from "react-router-dom";
+
+const VideoPlayer = ({}) => {
+let {videoId} = useParams();
+
   return (
     <div>
       <iframe
         id="ytplayer"
-        type="text/html"
         width="640"
         height="360"
-        src={`/video/${videoId}`}
+        src={`https://www.youtube.com/embed/${videoId}`}
         frameborder="0"
       ></iframe>
     </div>
