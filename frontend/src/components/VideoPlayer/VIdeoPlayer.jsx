6f7780b12b4539e698f7bb0df204b432a55a782c
Aubrey Guishard <aubrey.guishard@gmail.com>
2023-03-30 14:08:18 -0400

    Updated the Video Player and Video page with video title and description

--- frontend/src/components/VideoPlayer/VIdeoPlayer.jsx
+++ frontend/src/components/VideoPlayer/VIdeoPlayer.jsx
@@ -1,14 +1,16 @@
 import { useParams } from "react-router-dom";
+import React from 'react';
 
-const VideoPlayer = ({videoId}) => {
-// let {videoId} = useParams();
 
+const VideoPlayer = ({videoId, video}) => {
+console.log('The video',video)
   return (
     <div className='video-player'>
       <iframe
         src={`https://www.youtube.com/embed/${videoId}`}
       ></iframe>
-      <h3></h3>
+      <h3>{video.snippet.title}</h3>
+      <p>video description</p>
     </div>
   );
 };
--- frontend/src/pages/VideoPage/VideoPage.jsx
+++ frontend/src/pages/VideoPage/VideoPage.jsx
@@ -1,10 +1,24 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import VideoPlayer from "../../components/VideoPlayer/VIdeoPlayer";
 
 const VideoPage = ({}) => {
   const { videoId } = useParams();
+  const [video, setVideo] = useState(null)
+
+  async function fetchVIdeo(){
+    const api_key = "AIzaSyACDkhVL41ty7K077xwrNiTYj6KKsdizoQ"
+    const response = await axios.get(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${api_key}`)
+  setVideo('Video stuff',response.data.items[0]);
+}
+
+  useEffect(()=>{
+    fetchVIdeo()
+  }, [videoId]) 
+
   console.log(videoId);
-  return <div><VideoPlayer/></div>;
+  return video && (<div><VideoPlayer videoId={videoId} video={video}/></div>);
 };
 
 export default VideoPage;
